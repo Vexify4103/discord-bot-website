20253d14af7346996e76da32274c3019b661ee84
Vexify <painmain.shadow@gmail.com>
2024-02-04 19:50:51 +0100

    testing4

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,26 +8,7 @@ const Footer = () => {
 		<div>
 			<footer className="footer">
 				<div className="footer-sec1">
-					<div className="footer-header">
-						<ul>
-							<li>
-								<Link className="h-color" href="/">
-									bot.gg
-								</Link>
-								<span
-									style={{
-										fontSize: '12px',
-										display: 'flex',
-										flexDirection: 'row',
-										alignItems: 'center',
-									}}
-									className="p-color"
-								>
-									<Image src={botOnlineStatus} width={20} height={20} alt="online-status" className="onlineStatus" /> Bot is online!
-								</span>
-							</li>
-						</ul>
-					</div>
+					<div className="footer-header"></div>
 					<div className="footer-items">
 						<ul>
 							<li>
